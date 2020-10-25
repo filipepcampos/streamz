188dd1bcb641f6b2b3ba08aec37c40a9215e57b0
Francisco Cerqueira <57841600+xico2001pt@users.noreply.github.com>
2020-10-25 11:36:59 +0000

    Update User files

--- src/User.cpp
+++ src/User.cpp
@@ -1,1 +1,22 @@
 #include "User.h"
+#include <iostream>
+
+User::User(const std::string &nickname, const std::string &name, const Date &birth_date) : nickname(nickname), name(name), birth_date(birth_date) {
+}
+
+std::string User::getNickname() const {
+    return nickname;
+}
+
+std::string User::getName() const {
+    return name;
+}
+
+Date User::getBirthDate() const {
+    return birth_date;
+}
+
+void User::show() {
+    /* IMPLEMENTAR MAIS TARDE */
+    std::cout << "IMPLEMENTAR MAIS TARDE" << std::endl;
+}
--- src/User.h
+++ src/User.h
@@ -1,10 +1,49 @@
 #ifndef STREAMZ_USER_H
 #define STREAMZ_USER_H
+#include "Date.h"
+#include <string>
 
 
 class User {
-
+    /**
+     * Nickname of the user
+     */
+    std::string nickname;
+    /**
+     * Name of the user
+     */
+    std::string name;
+    /**
+     * Birth date of the user
+     */
+    Date birth_date;
+public:
+    /**
+     * Class constructor
+     * @param nickname Nickname of the user
+     * @param name Name of the user
+     * @param birth_date Birth date of the user
+     */
+    User(const std::string &nickname, const std::string &name, const Date &birth_date);
+    /**
+     * Gets the nickname of the user
+     * @return nickname of the user
+     */
+    std::string getNickname() const;
+    /**
+     * Gets the name of the user
+     * @return name of the user
+     */
+    std::string getName() const;
+    /**
+     * Gets the birth date of the user
+     * @return birth date of the user
+     */
+    Date getBirthDate() const;
+    /**
+     * Displays on the screen the info about the user
+     */
+    virtual void show() = 0;
 };
-
 
 #endif //STREAMZ_USER_H
